2e44d0a76619b5918b9d123676d45c2d73d8123d
Jaiden DeChon <jaiden.dechon@beast-code.com>
2022-02-12 16:13:53 -0600

    Removed HelloWorld component and implement skeleton for TopBar component

---
+++ client/src/components/TopBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>{{ header }}</div>
+</template>
+
+<script lang="ts">
+
+    import { Options, Vue } from 'vue-class-component';
+
+    @Options({
+        props: {
+            header: String
+        }
+    })
+
+    export default class TopBar extends Vue {
+        header!: string
+    }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
