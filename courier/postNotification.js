a5c8c5ec00ef387f6033f8e7aaff3b9dd7e83e94
Jaiden DeChon <jaiden.dechon@gmail.com>
2022-02-14 19:23:10 -0600

    Corrected another field that should have been modified

--- courier/postNotification.js
+++ courier/postNotification.js
@@ -4,7 +4,7 @@ const { CourierClient } = pkg;
 const courier = CourierClient({ authorizationToken: "pk_prod_SMC9QRNYT3MPZNMGXHX07J6JNDGG" });
 
 export default async function sendNotification (data) {
-    const profile = { email: 'jdechon1995@gmail.com'};
+    const profile = { email: 'jaiden.dechon.courier@gmail.com'};
     const { messageId } = await courier.send({
         brand: "BAT1B0YFP2MBFWJ8FTH1SBQBM5AV",
         eventId: "BY1PVVKZ2Q4TD2QPR2VRMWRP2C92",
