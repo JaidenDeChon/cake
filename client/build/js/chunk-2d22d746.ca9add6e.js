416c547d6e58064b455b3af71ef5666fc5bb0112
Jaiden DeChon <jaiden.dechon@gmail.com>
2022-05-18 16:24:07 -0500

    wiping project to begin anew

--- client/build/js/chunk-2d22d746.ca9add6e.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23"),e={class:"about"},s=Object(c["h"])("h1",null,"This is an about page",-1),u=[s];function o(n,t){return Object(c["s"])(),Object(c["g"])("div",e,u)}var b=a("6b0d"),d=a.n(b);const i={},r=d()(i,[["render",o]]);t["default"]=r}}]);
-//# sourceMappingURL=chunk-2d22d746.ca9add6e.js.map
