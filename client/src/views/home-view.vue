9aa03cd3eb16b1cd40a1f8d03bdda5b05e07d5fc
Jaiden DeChon <jaiden.dechon@gmail.com>
2022-06-09 23:37:30 -0500

    App.vue and home-view.vue now consume BlogPost data from API and store.

--- client/src/views/home-view.vue
+++ client/src/views/home-view.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
+
+    import { ref } from 'vue';
+    import type BlogPost from '../../../models/BlogPost';
+    import BlogPostComponent from '@/components/blog-post.vue';
+    import { useBlogsStore } from '@/stores/blogs';
+
+    const blogStore = useBlogsStore();
+    const blogPosts: BlogPost[] = blogStore.posts;
+    const blogs = ref(blogPosts);
+
 </script>
 
 <template>
-    <div></div>
+    <BlogPostComponent v-for="blog in blogs" :key="blog.title" :blog-post="blog" />
 </template>
