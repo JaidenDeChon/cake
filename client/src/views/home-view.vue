1c2b6d2354182593390db2c392ac0945d2939f5b
Jaiden DeChon <jdechon1995@gmail.com>
2022-06-10 00:39:53 -0500

    Consumed blog-post.vue

--- client/src/views/home-view.vue
+++ client/src/views/home-view.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
 
-    import { ref } from 'vue';
-    import type BlogPost from '../../../models/BlogPost';
+    import { ref, computed } from 'vue';
     import BlogPostComponent from '@/components/blog-post.vue';
     import { useBlogsStore } from '@/stores/blogs';
 
     const blogStore = useBlogsStore();
-    const blogPosts: BlogPost[] = blogStore.posts;
-    const blogs = ref(blogPosts);
+    const blogs = computed(() => blogStore.posts);
 
 </script>
 
 <template>
-    <BlogPostComponent v-for="blog in blogs" :key="blog.title" :blog-post="blog" />
+    <div class="home-content">
+        <BlogPostComponent v-for="blog in blogs" :key="blog.title" :blog-post="blog" />
+    </div>
 </template>
+
+<style lang="scss" scoped>
+
+    .home-content {
+        padding: 4rem var(--global-padding);
+    }
+
+</style>
