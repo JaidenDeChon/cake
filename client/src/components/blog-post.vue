85c6133540bd86eefa158c87dbc1c066d9c23901
Jaiden DeChon <jdechon1995@gmail.com>
2022-06-10 00:39:22 -0500

    Got blog-post.vue working

--- client/src/components/blog-post.vue
+++ client/src/components/blog-post.vue
@@ -1,7 +1,8 @@
 <template>
     <article class="article">
-        <img class="article__img" :src="blogPost.title">
-        <h1 class="article__title">{{ blogPost.title }}</h1>
+        <img class="article__img" :src="blogPost.img">
+        <h3 class="article__title">{{ blogPost.title }}</h3>
+        <p class="article__date">{{ blogPost.date }}</p>
         <p class="article__content">{{ blogPost.content }}</p>
     </article>
 </template>
@@ -16,9 +17,33 @@
         blogPost: { type: Object as PropType<BlogPost>, required: true }
     });
 
-    const blogPost = ref(props);
+    const blogPost = ref(props.blogPost);
 
 </script>
 
 <style lang="scss" scoped>
+
+    .article {
+        width: 100%;
+    
+        display: flex;
+        align-items: flex-start;
+        justify-content: center;
+        flex-direction: column;
+
+        &__img,
+        &__title,
+        &__date,
+        &__content {
+            margin-bottom: 0.4rem;
+        }
+
+        &__img {
+            width: 100%;
+            max-height: 30rem;
+
+            object-fit: cover;
+        }
+    }
+
 </style>
