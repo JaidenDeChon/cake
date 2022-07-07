cfb93c895797fe2ac4a427d9cba2774fc4e439bf
Jaiden DeChon <jdechon1995@gmail.com>
2022-07-06 21:45:19 -0500

    Improved Blog Post styles

--- client/src/components/blog-post.vue
+++ client/src/components/blog-post.vue
@@ -1,7 +1,7 @@
 <template>
     <article class="article">
         <img class="article__img" :src="blogPost.img">
-        <h3 class="article__title">{{ blogPost.title }}</h3>
+        <h2 class="article__title">{{ blogPost.title }}</h2>
         <p class="article__date">{{ blogPost.date }}</p>
         <p class="article__content">{{ blogPost.content }}</p>
     </article>
@@ -32,22 +32,28 @@
         justify-content: center;
         flex-direction: column;
 
+        margin-bottom: 2.2rem;
+
         &__title,
         &__date,
         &__content {
-            margin-bottom: 0.4rem;
+            margin-bottom: 0.2rem;
         }
 
         &__img {
             width: 100%;
             max-height: 30rem;
 
-            margin-bottom: 2.2rem;
+            margin-bottom: 0.8rem;
 
             object-fit: cover;
 
             border-radius: 0.5rem;
             box-shadow: rgba(100, 100, 111, 0.2) 0rem 0.4375rem 1.8125rem 0;
+        }
+
+        &__date {
+            color: var(--color-border-hover)
         }
     }
 
