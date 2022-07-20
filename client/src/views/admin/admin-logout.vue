66326aef74d8498e79ddd134aab0e379ca19f2b6
Jaiden DeChon <jdechon1995@gmail.com>
2022-07-20 00:00:40 -0500

    Created admin-logout view which logs the user out

---
+++ client/src/views/admin/admin-logout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import { onBeforeMount } from 'vue';
+    import { useRouter } from 'vue-router';
+
+    import { useAuthenticationStore } from '../../stores/authentication';
+    import { routeNames } from '@/router';
+
+    const authenticationStore = useAuthenticationStore();
+    const router = useRouter();
+
+    onBeforeMount(() => {
+        // todo - actually deauthenticate
+        authenticationStore.userIsAuthenticated = false;
+        router.push({ name: routeNames.HOME });
+    });
+
+</script>
+
+<template>
+    <div class="logout"></div>
+</template>
