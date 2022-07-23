d209aa0e562d3a545f85d83e0a0424dd01529500
Jaiden DeChon <jdechon1995@gmail.com>
2022-07-23 00:52:20 -0500

    Began modifying blog-post.vue to support an edit mode

--- client/src/components/blog-post.vue
+++ client/src/components/blog-post.vue
@@ -1,24 +1,109 @@
-<template>
-    <article class="article">
-        <img class="article__img" :src="blogPost.img">
-        <h2 class="article__title">{{ blogPost.title }}</h2>
-        <p class="article__date">{{ blogPost.date }}</p>
-        <p class="article__content">{{ blogPost.content }}</p>
-    </article>
+<template lang="pug">
+
+.article(:class="{ 'article--edit-mode': isModifiable && currentlyEditing }")
+
+    template(v-if="currentlyEditing")
+
+        label.admin-config-form__label Title
+            input.admin-config-form__input(v-model="blogPostUpdate.title")
+
+        label.admin-config-form__label (Optional) Image
+            input.admin-config-form__input(v-model="blogPostUpdate.img")
+
+        label.admin-config-form__label Content
+            textarea.admin-config-form__input(v-model="blogPostUpdate.content")
+
+        .article__edit-controls
+
+            button.jaid-button(
+                :disabled="!changesHaveBeenMade || currentlySaving"
+                @click="saveChanges"
+            ) {{ saveButtonText }}
+
+    template(v-else)
+
+        img.article__img(v-if="blogPost.img" :src="blogPost.img")
+        h2.article__title {{ blogPost.title }}
+        p.article__date {{ blogPost.date }}
+        p.article__content(v-if="showContent") {{ blogPost.content }}
+
+        .article__edit-controls(v-if="isModifiable && !currentlyEditing")
+
+            button.jaid-button(
+                :disabled="!changesHaveBeenMade || currentlySaving"
+                @click="enableEditMode"
+            ) {{ editButtonText }}
+
+            button.jaid-button(
+                :disabled="!changesHaveBeenMade || currentlySaving"
+                @click="saveChanges"
+            ) {{ deleteButtonText }}
+
 </template>
 
 <script setup lang="ts">
 
-    import type { PropType } from 'vue';
-    import { ref } from 'vue';
+    import { onMounted, ref, type PropType } from 'vue';
+    import { computed } from '@vue/reactivity';
+    import { isEqual } from 'lodash';
 
     import type { IBlogPost } from '@models/';
 
+    /** Set up props. */
+
     const props = defineProps({
-        blogPost: { type: Object as PropType<IBlogPost>, required: true }
+        blogPost: { type: Object as PropType<IBlogPost>, required: true },
+        isModifiable: { type: Boolean, required: false },
+        showContent: { type: Boolean, required: false, default: true }
     });
 
+    /** Set up emits. */
+
+    const emitToParent = defineEmits(['update-blog-post']);
+
+    /** Lifecycle hooks. */
+
+    onMounted(() => setupBlogPostUpdateVModels());
+
+    /** Blog Post-related functionality. */
+
     const blogPost = ref(props.blogPost);
+
+    /** Blog Post configuration-related functionality.  */
+
+    const blogPostUpdate = {} as IBlogPost;
+
+    const currentlyEditing = ref(false);
+    const currentlySaving = ref(false);
+    const currentlyDeleting = ref(false);
+
+    const editButtonText = 'Edit Blog Post';
+    const deleteButtonText = computed(() => currentlyDeleting.value ? 'Deleting...' : 'Delete Blog Post');
+    const saveButtonText = computed(() => currentlySaving.value ? 'Saving...' : 'Save Changes');
+    const changesHaveBeenMade = computed(() => !isEqual(blogPost, blogPostUpdate));
+
+    function enableEditMode (): void {
+        currentlyEditing.value = true;
+    }
+
+    /**
+     * Updates the v-model object of this component to make editing a smoother process.
+     */
+    function setupBlogPostUpdateVModels () {
+        blogPostUpdate.id = props.blogPost.id;
+        blogPostUpdate.title = props.blogPost.title;
+        blogPostUpdate.content = props.blogPost.content;
+        blogPostUpdate.date = props.blogPost.date;
+        blogPostUpdate.img = props.blogPost.img;
+    }
+
+    /**
+     * Emits to the parent that the changes to the Blog Post should be saved.
+     */
+    function saveChanges () {
+        emitToParent('update-blog-post');
+        currentlyEditing.value = false;
+    }
 
 </script>
 
@@ -54,6 +139,26 @@
 
         &__date {
             color: var(--color-border-hover)
+        }
+
+        &__content {
+            margin-bottom: var(--global-padding);
+        }
+
+        &__edit-controls {
+            margin-top: calc(var(--global-padding) / 2);
+            button:not(:last-child) {
+                margin-right: var(--global-padding)
+            }
+        }
+
+        &--edit-mode {
+            label {
+                display: flex;
+                flex-direction: column;
+
+                width: 100%;
+            }
         }
     }
 
