e495caaf9d4998303caa1fb61bb13a36ae15fb98
Jaiden DeChon <jdechon1995@gmail.com>
2022-10-23 22:19:39 -0500

    Component quill-editor is now capable of uploading and images to server

---
+++ client/src/api/imagesApi.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+import type { IHostedImage } from '@models/';
+
+/**
+     * Uploads the given image file.
+     * @param   { FormData }   formData   A FormData object containing the file to be uploaded.
+     * @returns an object with metadata representing the upload.
+     */
+export async function uploadImage (formData: FormData): Promise<IHostedImage> {
+    const result = await axios.post('api/images/upload-image', formData);
+    return result.data;
+}
--- client/src/components/quill-editor.vue
+++ client/src/components/quill-editor.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 
 import { onActivated, onMounted, ref, type PropType } from 'vue';
-
 import Quill, { type QuillOptionsStatic } from 'quill';
 
+import { uploadImage } from '@/api/imagesApi';
+import type { IHostedImage } from '@models/';
+
+// Set up component props.
 const props = defineProps(
     {
         disabled: { type: Boolean, default: true },
@@ -11,48 +14,113 @@ const props = defineProps(
     }
 )
 
-const editorElement = ref(null as null | Element);
-let editor!: Quill;
-
+// Set up component emits.
 const $emit = defineEmits(['contents-changed']);
 
+// Set up QuillJS-related variables.
+let editor!: Quill;
+const editorElement = ref(null as null | Element);
 const editorOptions: QuillOptionsStatic = {
     debug: 'error',
     placeholder: 'Blog post content here...',
     theme: 'snow',
     modules: {
         toolbar: [
-            ['bold', 'italic', 'underline'],
-            [{ list: 'ordered' }, { list: 'bullet' }]
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ list: 'ordered' }, { list: 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+            [{ 'color': [] }, { 'background': [] }],
+            ['link', 'image']
         ]
     }
 };
 
-onMounted(() => {
+// Vue `mounted` lifecycle hook.
+onMounted(() => { buildQuill(); });
 
+// Vue `activated` lifecycle hook.
+onActivated(() => setQuillContents());
+
+/**
+ * Sets up the Quill editor. Disables if `disabled` prop is true.
+ */
+function buildQuill (): void {
     editor = new Quill(editorElement.value as Element, editorOptions);
-
-    setContents();
+    setQuillContents();
 
     if (props.disabled) {
         editor.disable();
         return;
     }
 
+    editor.getModule('toolbar').addHandler('image', () => { selectLocalImage(); });
     editor.on('text-change', emitContentsChanged);
-});
-
-onActivated(() => setContents());
-
-function setContents () {
-    if (props.contents) {
-        // TODO - Figure out Delta type for this error
-        editor.setContents(props.contents);
-    }
 }
 
+/**
+ * Passes the `contents` prop into the Quill instance.
+ */
+function setQuillContents () {
+    if (props.contents) editor.setContents(props.contents);
+}
+
+/**
+ * Informs the parent of a change in the content of the Quill instance.
+ */
 function emitContentsChanged (): void {
     $emit('contents-changed', editor.getContents());
+}
+
+/**
+ * Prompts the user to select an image for use in this Quill document. Once chosen, the image is uploaded
+ * to the server and then placed within the editor.
+ */
+async function selectLocalImage (): Promise<void> {
+
+    // Create and click an input element to prompt the user to select a file.
+    const input: HTMLInputElement = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.click();
+
+    // Listen for when the user has selected a file.
+    input.onchange = async () => {
+
+        // Ensure there is a file before continuing.
+        if (!input.files || !input.files[0]) return;
+
+        const file: File = input.files[0];
+
+        // Ensure the file is an image.
+        if (!/^image\//.test(file.type)) alert('You may only upload images.');
+
+        // Upload the image and then place it within the editor.
+        const hostedImageData = await sendImageToServer(file);
+        insertImageWithinQuill(hostedImageData);
+    }
+
+    // Remove the newly-created input element from the DOM as we no longer need it.
+    input.remove();
+}
+
+/**
+ * Sends an image, selected by the user, to the server for storage.
+ * @param   { File }   file   The file that is to be uploaded for storage.
+ */
+async function sendImageToServer (file: File): Promise<IHostedImage> {
+    const formData = new FormData();
+    formData.append('file', file);
+    const returnValue = await uploadImage(formData);
+    return returnValue;
+}
+
+/**
+ * Uses the URL within an IHostedImage object to place the user's selected image into the Quill instance.
+ * @param   { IHostedImage }   hostedImageData   An object containing information about the uploaded image.
+ */
+function insertImageWithinQuill (hostedImageData: IHostedImage): void {
+    let currentIndex = editor.getSelection()?.index;
+    if (currentIndex === undefined) currentIndex = 0;
+    editor.insertEmbed(currentIndex, 'image', hostedImageData.url);
+    editor.setSelection(currentIndex + 1, 0);
 }
 
 </script>
@@ -90,7 +158,7 @@ function emitContentsChanged (): void {
         }
 
         .ql-toolbar {
-            height: 2.5rem;
+            min-height: 2.5rem;
             display: flex;
             border-bottom: none !important;
             border-radius: 
