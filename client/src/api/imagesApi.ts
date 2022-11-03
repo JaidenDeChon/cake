dce9f32697ffa21585dd3a9d85ed928c2a387606
Jaiden DeChon <jdechon1995@gmail.com>
2022-11-03 00:44:19 -0500

    Modified imagesApi.ts to support a config for status updates

--- client/src/api/imagesApi.ts
+++ client/src/api/imagesApi.ts
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
 import type { IHostedImage } from '@models/';
+import type { Ref } from 'vue';
 
 /**
      * Uploads the given image file.
      * @param   { FormData }   formData   A FormData object containing the file to be uploaded.
      * @returns an object with metadata representing the upload.
      */
-export async function uploadImage (formData: FormData): Promise<IHostedImage> {
-    const result = await axios.post('api/images/upload-image', formData);
+export async function uploadImage (formData: FormData, progress?: Ref<number>): Promise<IHostedImage> {
+    const config = {
+        onUploadProgress: (progressEvent: ProgressEvent) => {
+            if (progress !== undefined) progress.value = progressEvent.loaded;
+        }
+    };
+    const result = await axios.post('api/images/upload-image', formData, config);
     return result.data;
 }
