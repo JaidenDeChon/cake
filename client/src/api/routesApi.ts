61794bdb0244263c06eec36fcd30b8ee1c8a4f17
Jaiden DeChon <jaiden.dechon@gmail.com>
2023-04-10 16:43:49 -0500

    Refactored admin-config-routes to support editing alongside creating

--- client/src/api/routesApi.ts
+++ client/src/api/routesApi.ts
@@ -35,8 +35,9 @@ export async function getRoute (id: string): Promise<IJaidRoute> {
  *                                              update the database.
  */
 export async function updateRoute (newData: Partial<IJaidRoute>): Promise<IJaidRoute> {
-    if (!newData.id) throw new Error('Attempted to update a route without a valid ID.');
-    const result = await axios.patch(`api/routes/update-route/${ newData.id?.toString() }`, newData);
+    console.log(newData);
+    if (!newData._id) throw new Error('Attempted to update a route without a valid ID.');
+    const result = await axios.patch(`api/routes/update-route/${ newData._id?.toString() }`, newData);
     return result.data;
 }
 
--- client/src/views/admin/admin-config-routes-modal.vue
+++ client/src/views/admin/admin-config-routes-modal.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 
+    import { computed } from 'vue';
+
+    import type { IJaidRoute } from '@models/';
     import QuillEditorComponent from '../../components/quill-editor.vue';
     import modalBase from '@/components/modal-base.vue';
 
     export interface Props {
         visible: boolean;
-        pageContent?: object;
+        jaidRoute?: IJaidRoute;
         header?: string;
         subheader?: string;
     }
@@ -20,6 +23,10 @@
 
     const $emit = defineEmits<Emits>();
 
+    const titleComputed = computed(() => $props.jaidRoute?.pageTitle ?? undefined);
+
+    const quillContentComputed = computed(() => $props.jaidRoute?.content ?? undefined)
+
 </script>
 
 <template>
@@ -32,11 +39,19 @@
             <!-- Title input. -->
             <label class="admin-config-form__label">
                 Title
-                <input class="admin-config-form__input" @input="$emit('update-title', ($event.target as any).value ?? 'New Page')" />
+                <input
+                    :value="titleComputed"
+                    class="admin-config-form__input"
+                    @input="$emit('update-title', ($event.target as any).value ?? 'New Page')"
+                />
             </label>
 
             <!-- Editor for quill content. -->
-            <quill-editor-component :disabled="false" @contents-changed="$emit('contents-changed', $event)" />
+            <quill-editor-component
+                :disabled="false"
+                :contents="quillContentComputed"
+                @contents-changed="$emit('contents-changed', $event)"
+            />
 
             <!-- Submit button. -->
             <button class="jaid-button" @click="$emit('save')">Done</button>
